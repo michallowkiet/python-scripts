f7d13bc0732a2dee01e632cd27615535d8321989
Michał Łowkiet <michallowkiet@gmail.com>
2024-12-06 12:20:30 +0100

    Hacker news article scraper

--- web_scrape/scrape.py
+++ web_scrape/scrape.py
@@ -2,14 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://news.ycombinator.com/news"
-r = requests.get(url)
-html_doc = r.text
-
-soup = BeautifulSoup(html_doc, "html.parser")
-
-submissions = soup.select(".submission")
-
-subtext = soup.select(".subtext")
 
 
 def create_custom_hn(submissions, subtext):
@@ -25,15 +17,35 @@ def create_custom_hn(submissions, subtext):
     return hn
 
 
+def get_page():
+    r = requests.get(url)
+    html_doc = r.text
+    soup = BeautifulSoup(html_doc, "html.parser")
+    submissions = soup.select(".submission")
+    subtext = soup.select(".subtext")
+    next_page_url = soup.select(".morelink")[0].get("href")
+    return {"hn": create_custom_hn(submissions, subtext), "next_page_url": f"{url}{next_page_url}"}
+
+
+def get_next_page(next_page_url):
+    res = requests.get(f"{url}{next_page_url}")
+    html_doc = res.text
+    soup = BeautifulSoup(html_doc, "html.parser")
+    submissions = soup.select(".submission")
+    subtext = soup.select(".subtext")
+    return create_custom_hn(submissions, subtext)
+
+
 def sort_stories_by_votes(hn):
     return sorted(hn, key=lambda k: k["votes"], reverse=True)
 
 
-hn = create_custom_hn(submissions, subtext)
-hn = sort_stories_by_votes(hn)
+hn = get_page()
+hn_sorted = sort_stories_by_votes(hn["hn"])
 
-for item in hn:
+for item in hn_sorted:
     print(item["title"], "\n")
     print(item["link"], "\n")
     print(item["votes"], "\n")
     print("\n")
+print(hn["next_page_url"])
