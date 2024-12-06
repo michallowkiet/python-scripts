4c8d6b687ecdda3092a8dbe24c4cc143a0f74dd7
Michał Łowkiet <michallowkiet@gmail.com>
2024-12-06 08:41:53 +0100

    Passwoed checker

--- password-checker/password-checker.py
+++ password-checker/password-checker.py
@@ -1,4 +1,5 @@
 import hashlib
+import sys
 
 import requests
 
@@ -11,17 +12,35 @@ def requests_api_data(query_char):
     return response
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = requests_api_data(first5_char)
-    match = (line for line in response.text.splitlines() if line.split(":")[0] == tail)
-    print(f"sh1: {sha1password}", "\n")
-    print(f"tail: {tail}", "\n")
-    for line in match:
-        print(line, "\n")
 
-    return response
+    return get_password_leaks_count(response, tail)
 
 
-pwned_api_check("123456")
+def main(args):
+    if args[0] == "-f" or args[0] == "--file":
+        with open(args[1], "r") as f:
+            args = f.readlines()
+
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f"{password} was found {count} times... you should probably change your password")
+        else:
+            print(f"{password} was not found. Carry on!")
+    return "Done!"
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
